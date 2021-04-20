158da0fb703a17dddfd0c0e924e00fa25b5621c5
Aman848 <anugill4080@gmail.com>
2021-04-20 14:54:42 -0400

    Aman data 61

--- app/src/main/java/com/example/marsapp/mlkit/MLKitVisionImage.java
+++ app/src/main/java/com/example/marsapp/mlkit/MLKitVisionImage.java
@@ -1,4 +1,28 @@
 package com.example.marsapp.mlkit;
 
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 public class MLKitVisionImage {
+
+    private static final String TAG = "MLKIT";
+    private static final String MY_CAMERA_ID = "my_camera_id";
+
+    private void imageFromBitmap(Bitmap bitmap) {
+        int rotationDegree = 0;
+        // [START image_from_bitmap]
+        InputImage image = InputImage.fromBitmap(bitmap, rotationDegree);
+        // [END image_from_bitmap]
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void imageFromMediaImage(Image mediaImage, int rotation) {
+        // [START image_from_media_image]
+        InputImage image = InputImage.fromMediaImage(mediaImage, rotation);
+        // [END image_from_media_image]
+    }
+
 }
