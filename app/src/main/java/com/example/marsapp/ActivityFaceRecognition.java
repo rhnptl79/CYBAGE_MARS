7a781a891509ccfcfbcb4dcdd5fe43501fc2f2e0
lenovo <sanjayprajapati92@gmail.com>
2021-04-20 16:24:18 +0530

    face 29

--- app/src/main/java/com/example/marsapp/ActivityFaceRecognition.java
+++ app/src/main/java/com/example/marsapp/ActivityFaceRecognition.java
@@ -2,9 +2,11 @@ package com.example.marsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -39,5 +41,63 @@ public class ActivityFaceRecognition extends AppCompatActivity {
         setContentView(R.layout.activity_face_recognition);
         preferences = new MyPreferences(this);
         initComponents();
+    }
+    private void initComponents() {
+
+        oriImage=(ImageView)findViewById(R.id.image1);
+        testImage=(ImageView)findViewById(R.id.image2);
+        buverify=(Button)findViewById(R.id.verify);
+        result_text=(TextView)findViewById(R.id.result);
+//        Uri myUri = Uri.fromFile(new File(preferences.getString(MyPreferences.USER_IMAGE)));
+//        try {
+//            Bitmap oribitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), myUri);
+//            oriImage.setImageBitmap(oribitmap);
+//
+//            face_detector(oribitmap,"original");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        try{
+            tflite=new Interpreter(loadmodelfile(this));
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        oriImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"),12);
+            }
+        });
+
+        testImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"),13);
+
+            }
+        });
+
+        buverify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                double distance=calculate_distance(ori_embedding,test_embedding);
+
+                if(distance<6.0)
+                    result_text.setText("Result : Same Faces");
+                else
+                    result_text.setText("Result : Different Faces");
+
+            }
+
+        });
+
     }
 }
