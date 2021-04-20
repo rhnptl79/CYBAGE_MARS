f37dfd189edc32efdbbeb1e3292af1962f55f099
Aman848 <anugill4080@gmail.com>
2021-04-20 14:39:32 -0400

    Aman data 52

--- app/src/main/java/com/example/marsapp/data/PurchasePrefData.java
+++ app/src/main/java/com/example/marsapp/data/PurchasePrefData.java
@@ -1,4 +1,27 @@
 package com.example.marsapp.data;
 
+import java.util.ArrayList;
+
 public class PurchasePrefData {
+    String email;
+
+    public void setCourseList(ArrayList<String> courseList) {
+        this.courseList = courseList;
+    }
+
+    ArrayList<String> courseList;
+
+
+    public PurchasePrefData(String email, ArrayList<String> courseList) {
+        this.email = email;
+        this.courseList = courseList;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public ArrayList<String> getCourseList() {
+        return courseList;
+    }
 }
