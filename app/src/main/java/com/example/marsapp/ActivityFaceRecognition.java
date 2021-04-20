14f4a583a3db56dc3eec305ba6fa37d0e452b807
lenovo <sanjayprajapati92@gmail.com>
2021-04-20 16:22:43 +0530

    face 28

--- app/src/main/java/com/example/marsapp/ActivityFaceRecognition.java
+++ app/src/main/java/com/example/marsapp/ActivityFaceRecognition.java
@@ -2,13 +2,42 @@ package com.example.marsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ActivityFaceRecognition extends AppCompatActivity {
+
+    protected Interpreter tflite;
+    private  int imageSizeX;
+    private  int imageSizeY;
+
+    private static final float IMAGE_MEAN = 0.0f;
+    private static final float IMAGE_STD = 1.0f;
+
+    public Bitmap oribitmap,testbitmap;
+    public static Bitmap cropped;
+    Uri imageuri;
+
+    ImageView oriImage,testImage;
+    Button buverify;
+    TextView result_text;
+
+    float[][] ori_embedding = new float[1][128];
+    float[][] test_embedding = new float[1][128];
+
+
+    ChoosePhotoHelper choosePhotoHelper;
+    MyPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_face_recognition);
+        preferences = new MyPreferences(this);
+        initComponents();
     }
 }
