62b67d0c19f05dd647e4dab5af2aee62042c8ed1
Aman848 <anugill4080@gmail.com>
2021-04-20 14:40:26 -0400

    Aman data 53

--- app/src/main/java/com/example/marsapp/data/UserData.java
+++ app/src/main/java/com/example/marsapp/data/UserData.java
@@ -1,4 +1,47 @@
 package com.example.marsapp.data;
 
+import java.util.ArrayList;
+
 public class UserData {
+    String strImagePath;
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+
+
+    ArrayList<CourseData> courseList;
+    public UserData(String strImagePath,String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.strImagePath=strImagePath;
+    }
+    public String getStrImagePath() {
+        return strImagePath;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public ArrayList<CourseData> getCourseList() {
+        return courseList;
+    }
+
+    public void setCourseList(ArrayList<CourseData> courseList) {
+        this.courseList = courseList;
+    }
 }
