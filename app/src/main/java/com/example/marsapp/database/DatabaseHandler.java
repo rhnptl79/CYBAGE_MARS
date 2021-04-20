6d7285cdb2a4c07811b278573a1bfbdcca2b8585
Aman848 <anugill4080@gmail.com>
2021-04-20 14:46:34 -0400

    Aman data 56

--- app/src/main/java/com/example/marsapp/database/DatabaseHandler.java
+++ app/src/main/java/com/example/marsapp/database/DatabaseHandler.java
@@ -1,6 +1,7 @@
 package com.example.marsapp.database;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -23,6 +24,27 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public DatabaseHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         //3rd argument to be passed is CursorFactory instance
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_COURSE_TABLE = "CREATE TABLE " + TABLE_COURSE + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_COURSE_NAME + " TEXT," + KEY_COURSE_IS_PAID +" BOOLEN," +KEY_COURSE_FEE +" TEXT" +")";
+
+        String CREATE_COURSE_CONTENT_TABLE = "CREATE TABLE " + TABLE_CONTENT + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_VIDEO_NAME + " TEXT," + KEY_VIDEO_URL +" TEXT," +KEY_COURSE_ID +" TEXT" +")";
+
+        db.execSQL(CREATE_COURSE_TABLE);
+        db.execSQL(CREATE_COURSE_CONTENT_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COURSE);
+
+        // Create tables again
+
+        onCreate(db);
     }
 
 }
