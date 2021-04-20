9638c020f4ad4bd09e617a1dfd9a21d3cd11f601
Aman848 <anugill4080@gmail.com>
2021-04-20 14:14:13 -0400

    Aman data

--- app/src/main/java/com/example/marsapp/data/CourseContentData.java
+++ app/src/main/java/com/example/marsapp/data/CourseContentData.java
@@ -1,4 +1,25 @@
 package com.example.marsapp.data;
 
 public class CourseContentData {
+    int courseId;
+    String name;
+    String url;
+
+    public CourseContentData(int courseId, String name, String url) {
+        this.courseId = courseId;
+        this.name = name;
+        this.url = url;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
