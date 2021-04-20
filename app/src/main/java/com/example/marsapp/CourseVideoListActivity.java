7a4899901cb8dccf10e78d1e66bc775d3b54685a
rhnptl79 <rhnptl79@gmail.com>
2021-04-20 14:02:19 +0530

    checking 4

---
+++ app/src/main/java/com/example/marsapp/CourseVideoListActivity.java
@@ -0,0 +1,14 @@
+package com.example.marsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CourseVideoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_video_list);
+    }
+}
