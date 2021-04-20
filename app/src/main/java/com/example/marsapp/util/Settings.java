e91c53ba6f5a2b054b3bd7300f7d2389d0dc07e6
Aman848 <anugill4080@gmail.com>
2021-04-20 15:13:28 -0400

    Aman data 71

--- app/src/main/java/com/example/marsapp/util/Settings.java
+++ app/src/main/java/com/example/marsapp/util/Settings.java
@@ -1,4 +1,65 @@
 package com.example.marsapp.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 public class Settings {
+
+    protected static final String ENVIRONMENT = "environment";
+
+    private static final String VERSION = "version";
+
+    private static final String SANDBOX_BASE_SERVER_URL = "https://braintree-sample-merchant.herokuapp.com";
+    private static final String PRODUCTION_BASE_SERVER_URL = "https://executive-sample-merchant.herokuapp.com";
+
+    private static final String SANDBOX_TOKENIZATION_KEY = "sandbox_tmxhyf7d_dcpspy2brwdjr3qn";
+    private static final String PRODUCTION_TOKENIZATION_KEY = "production_t2wns2y2_dfy45jdj3dxkmz5m";
+
+    private static SharedPreferences sSharedPreferences;
+
+    public static SharedPreferences getPreferences(Context context) {
+        if (sSharedPreferences == null) {
+            sSharedPreferences = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        }
+
+        return sSharedPreferences;
+    }
+
+    public static int getVersion(Context context) {
+        return getPreferences(context).getInt(VERSION, 0);
+    }
+
+    public static void setVersion(Context context) {
+        getPreferences(context).edit().putInt(VERSION, BuildConfig.VERSION_CODE).apply();
+    }
+
+    public static int getEnvironment(Context context) {
+        return getPreferences(context).getInt(ENVIRONMENT, 0);
+    }
+
+    public static void setEnvironment(Context context, int environment) {
+        getPreferences(context)
+                .edit()
+                .putInt(ENVIRONMENT, environment)
+                .apply();
+
+//        DemoApplication.resetApiClient();
+    }
+
+    public static String getSandboxUrl() {
+        return SANDBOX_BASE_SERVER_URL;
+    }
+
+    public static String getEnvironmentUrl(Context context) {
+        int environment = getEnvironment(context);
+        if (environment == 0) {
+            return SANDBOX_BASE_SERVER_URL;
+        } else if (environment == 1) {
+            return PRODUCTION_BASE_SERVER_URL;
+        } else {
+            return "";
+        }
+    }
+
 }
