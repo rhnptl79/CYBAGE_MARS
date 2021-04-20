a009585adfd207cad7fa9480429421f79962e6d9
rhnptl79 <rhnptl79@gmail.com>
2021-04-20 13:59:34 +0530

    checking 3

---
+++ app/src/main/java/com/example/marsapp/ActivityCertificate.java
@@ -0,0 +1,14 @@
+package com.example.marsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityCertificate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_certificate);
+    }
+}
