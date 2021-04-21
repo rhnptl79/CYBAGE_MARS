274459815556ea9beb3add389bccb586ae55815b
hp <rawatved04@gmail.com>
2021-04-21 01:33:59 -0400

    Mohit course 99

--- app/src/main/java/com/example/marsapp/adapter/AdapterCourseVideo.java
+++ app/src/main/java/com/example/marsapp/adapter/AdapterCourseVideo.java
@@ -1,13 +1,18 @@
 package com.example.marsapp.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.marsapp.R;
+import com.example.marsapp.VideoViewActivity;
 import com.example.marsapp.data.CourseContentData;
 
 import java.util.ArrayList;
@@ -27,5 +32,35 @@ public class AdapterCourseVideo extends RecyclerView.Adapter<AdapterCourseVideo.
         View listItem = layoutInflater.inflate(R.layout.adapter_course_list, parent, false);
 
         return new MyVideoHolder(listItem);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterCourseVideo.MyVideoHolder holder, int position) {
+        holder.tv_type.setVisibility(View.GONE);
+        holder.tv_name.setText(_list.get(position).getName());
+        holder.parentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(_context, VideoViewActivity.class);
+                intent.putExtra("url",_list.get(position).getUrl());
+                _context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return _list.size();
+    }
+
+    public class MyVideoHolder extends RecyclerView.ViewHolder {
+        TextView tv_name,tv_type;
+        ConstraintLayout parentView;
+        public MyVideoHolder(@NonNull View itemView) {
+            super(itemView);
+            tv_name=itemView.findViewById(R.id.tv_name);
+            tv_type=itemView.findViewById(R.id.tv_type);
+            parentView=itemView.findViewById(R.id.parentView);
+        }
     }
 }
