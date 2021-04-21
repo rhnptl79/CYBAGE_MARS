4daf4e3f519845403a2f56e73129d358f1100133
hp <rawatved04@gmail.com>
2021-04-21 00:52:19 -0400

    Mohit course 76

--- app/src/main/java/com/example/marsapp/CourseListActivity.java
+++ app/src/main/java/com/example/marsapp/CourseListActivity.java
@@ -1,10 +1,25 @@
 package com.example.marsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.marsapp.data.CourseData;
+import com.example.marsapp.database.DatabaseHandler;
+import com.example.marsapp.util.MyPreferences;
+
+import java.util.ArrayList;
 
 public class CourseListActivity extends AppCompatActivity {
+
+    RecyclerView rv_course_list;
+    AdapterCourseList adapter;
+    ArrayList<CourseData> data;
+    DatabaseHandler databaseHandler;
+    TextView tv_logOUt;
+    MyPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
