3dad1eec1faade2043684ad2a1e8a00c0dd7ee8e
hp <rawatved04@gmail.com>
2021-04-21 01:00:14 -0400

    Mohit course 83

--- app/src/main/java/com/example/marsapp/CourseVideoListActivity.java
+++ app/src/main/java/com/example/marsapp/CourseVideoListActivity.java
@@ -3,7 +3,9 @@ package com.example.marsapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -25,5 +27,32 @@ public class CourseVideoListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_video_list);
+        rv_course_list=findViewById(R.id.rv_course_list);
+        btn_back=findViewById(R.id.btn_back);
+        btn_take_test=findViewById(R.id.btn_take_test);
+
+        data=new ArrayList<>();
+        initView(data);
+        databaseHandler=new DatabaseHandler(this);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btn_take_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(CourseVideoListActivity.this,EntranceTestActivity.class);
+                intent.putExtra("from","videoActivity");
+                intent.putExtra("id", getIntent().getIntExtra("id",1));
+                intent.putExtra("course", getIntent().getStringExtra("name"));
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
