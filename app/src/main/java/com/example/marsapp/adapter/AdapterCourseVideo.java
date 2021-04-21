f143f7629c65398fc5b74151c40df8d62fd036f8
hp <rawatved04@gmail.com>
2021-04-21 01:31:57 -0400

    Mohit course 97

---
+++ app/src/main/java/com/example/marsapp/adapter/AdapterCourseVideo.java
@@ -0,0 +1,6 @@
+package com.example.marsapp.adapter;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterCourseVideo extends RecyclerView.Adapter<AdapterCourseVideo.MyVideoHolder> {
+}
