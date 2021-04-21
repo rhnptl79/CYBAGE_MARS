f66bdecac379a6c34ac6784c9401ce3420f71e10
hp <rawatved04@gmail.com>
2021-04-21 01:27:00 -0400

    Mohit course 94

--- app/src/main/java/com/example/marsapp/adapter/AdapterCourseList.java
+++ app/src/main/java/com/example/marsapp/adapter/AdapterCourseList.java
@@ -1,6 +1,7 @@
 package com.example.marsapp.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,6 +30,36 @@ public class AdapterCourseList extends RecyclerView.Adapter<AdapterCourseList.My
         View listItem = layoutInflater.inflate(R.layout.adapter_course_list, parent, false);
 
         return new AdapterCourseList.MyViewHolder(listItem);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterCourseList.MyViewHolder holder, int position) {
+        holder.tv_name.setText(_dataList.get(position).getCourseName());
+        holder.tv_type.setText(_dataList.get(position).getCourseFee());
+        if (_dataList.get(position).isPaid()) {
+            holder.parentView.setOnClickListener(v -> {
+                Intent intent;
+                if (_dataList.get(position).getCourseFee().equalsIgnoreCase("Purchased")) {
+                    intent = new Intent(_context, CourseVideoListActivity.class);
+                } else {
+                    intent = new Intent(_context, EntranceTestActivity.class);
+                }
+                intent.putExtra("id", _dataList.get(position).getCourseId());
+                intent.putExtra("name", _dataList.get(position).getCourseName());
+                intent.putExtra("fees", _dataList.get(position).getCourseFee().replace("$", ""));
+                _context.startActivity(intent);
+            });
+        } else {
+            holder.parentView.setOnClickListener(v -> {
+                Intent intent = new Intent(_context, CourseVideoListActivity.class);
+                intent.putExtra("id", _dataList.get(position).getCourseId());
+                intent.putExtra("name", _dataList.get(position).getCourseName());
+                intent.putExtra("fees", _dataList.get(position).getCourseFee().replace("$", ""));
+                _context.startActivity(intent);
+            });
+        }
+
+
     }
 
 }
