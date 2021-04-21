fd3d7f945171270b4381e7e5b1d5b96320cb59b5
hp <rawatved04@gmail.com>
2021-04-21 01:06:48 -0400

    Mohit course 87

--- app/src/main/java/com/example/marsapp/EntranceTestActivity.java
+++ app/src/main/java/com/example/marsapp/EntranceTestActivity.java
@@ -1,11 +1,25 @@
 package com.example.marsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.marsapp.data.EntranceTestData;
+
+import java.util.ArrayList;
 
 public class EntranceTestActivity extends AppCompatActivity {
 
+    ArrayList<EntranceTestData> qusData;
+    RecyclerView rv_question;
+    AdapterEntranceTest adapter;
+    Button btn_submit;
+    TextView tv_time;
+    ImageView btn_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
