05653b213db74cc36566d40a7e32f7621c89bce2
hp <rawatved04@gmail.com>
2021-04-21 01:32:42 -0400

    Mohit course 98

--- app/src/main/java/com/example/marsapp/adapter/AdapterCourseVideo.java
+++ app/src/main/java/com/example/marsapp/adapter/AdapterCourseVideo.java
@@ -1,6 +1,31 @@
 package com.example.marsapp.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.marsapp.data.CourseContentData;
+
+import java.util.ArrayList;
+
 public class AdapterCourseVideo extends RecyclerView.Adapter<AdapterCourseVideo.MyVideoHolder> {
+    ArrayList<CourseContentData> _list;
+    Context _context;
+    public AdapterCourseVideo(ArrayList<CourseContentData> list, Context context) {
+        _list=list;
+        _context=context;
+    }
+
+    @NonNull
+    @Override
+    public AdapterCourseVideo.MyVideoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem = layoutInflater.inflate(R.layout.adapter_course_list, parent, false);
+
+        return new MyVideoHolder(listItem);
+    }
 }
