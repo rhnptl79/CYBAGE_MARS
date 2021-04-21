3df858ef3d988e96411558ed4b6a8da2876bab63
hp <rawatved04@gmail.com>
2021-04-21 01:08:49 -0400

    Mohit course 88

--- app/src/main/java/com/example/marsapp/EntranceTestActivity.java
+++ app/src/main/java/com/example/marsapp/EntranceTestActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -24,5 +25,48 @@ public class EntranceTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrance_test);
+        rv_question = findViewById(R.id.rv_question);
+        btn_submit = findViewById(R.id.btn_submit);
+        tv_time = findViewById(R.id.tv_time);
+        btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        if (getIntent().hasExtra("from")) {
+            qusData = MyTestData.getFinalTestData(getIntent().getIntExtra("id", 1));
+        } else {
+            qusData = MyTestData.getEntranceData(getIntent().getIntExtra("id", 1));
+        }
+
+        initView();
+        startTimer(60);
+        btn_submit = findViewById(R.id.btn_submit);
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<EntranceTestData> data = adapter.getData();
+                int totatlQuestion = data.size();
+                int correctAns = 0;
+                for (int i = 0; i < totatlQuestion; i++) {
+                    if (data.get(i).getAnswer().equalsIgnoreCase(data.get(i).getUserSubmitAnswer())) {
+                        correctAns += 1;
+                    }
+                }
+                int totalPercentageObtain = 0;
+                totalPercentageObtain = (correctAns / totatlQuestion) * 100;
+
+                if (totalPercentageObtain >= 40) {
+                    isShowDialog(true, "You are pass!!");
+
+                } else {
+                    isShowDialog(false, "Sorry you failed!! please try again.");
+                }
+
+            }
+        });
     }
 }
